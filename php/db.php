24adeaf9a75160d34b17bf51c023d5c8ce94f01f
dreem70 <dreem707070bigworth@gmail.com>
2025-05-12 20:44:35 +0300

    db.php

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = "";     
-$dbname = "general";
+$servername = "sql103.infinityfree.com";
+$username = "if0_37680309"; 
+$password = "AFeltn3I9xaFP5W";     
+$dbname = "if0_37680309_online_tech";
 
 // الاتصال بقاعدة البيانات
 $conn = new mysqli($servername, $username, $password, $dbname);
